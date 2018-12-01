ab1564e53368119d1cadb7fd0340d23e566a76c0
Eemeli <eemeliruohio@gmail.com>
2018-12-01 18:15:47 +0200

    to VS

---
+++ articles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    #add thumbnail
+    #add author
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50] + '...'
+
+    #python manage.py makemigrations
+    #python manage.py migrate
